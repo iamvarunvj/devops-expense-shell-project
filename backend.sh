b89b2f8fefce996fc711104df297aa9dba50a656
iamvarunvj <vijay.mamidi@resmed.com.au>
2024-09-02 19:26:32 +0530

    Update

--- backend.sh
+++ backend.sh
@@ -60,5 +60,6 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading backend application code"
 
 cd /app
+rm -rf /app/* # remove the existing file indside the /app folder
 unzip /tmp/backend.zip
 VALIDATE $? "Extracting backend application code"
