f09251d546600172e207a011a0d3737dad2a199d
iamvarunvj <vijay.mamidi@resmed.com.au>
2024-09-02 19:03:08 +0530

    Update

--- backend.sh
+++ backend.sh
@@ -42,7 +42,7 @@ VALIDATE $? "Enable nodejs:20"
 
 dnf install nodejs -y&>>$LOG_FILE
 VALIDATE $? "Install nodejs"
-id expense
+id expense&>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "user expense is not created.. creating user expense"&>>$LOG_FILE
